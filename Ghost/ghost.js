33759efc5cafa538ee158ac5de4f5d94d37183ce
jadecroo <35170285+jadecroo@users.noreply.github.com>
2020-05-03 15:05:07 -0400

    Trigger Animation

--- Ghost/ghost.js
+++ Ghost/ghost.js
@@ -1,66 +1,13 @@
-/*// PART 1: CANVAS
-
-const canvas = document.querySelector('canvas');
-      const context = canvas.getContext('2d');
-
-      let width;
-      let height;
-
-      // set the number of canvas pixels, scaled for screen resolution
-      let pxScale = window.devicePixelRatio;
-
-      function setup() {
-        // full browser canvas
-        width = window.innerWidth;
-        height = window.innerHeight;
-
-        // fixed canvas size
-        // width = canvas.width;
-        // height = canvas.height;
-
-        // set the CSS display size
-        canvas.style.width = width + 'px';
-        canvas.style.height = height + 'px';
-
-        canvas.width = width * pxScale;
-        canvas.height = height * pxScale;
-
-        // normalize the coordinate system
-        context.scale(pxScale, pxScale);
-
-        draw();
-      }
-
-      function draw() {
-
-        let opacity = 1;
-
-
-        for (let i = 0; i < 10; i++) {
-          opacity -= 0.1;
-          console.log(opacity);
-
-          if (opacity == 0) {
-            opacity = 1;
-          }
-        }
-
-        context.fillStyle = 'hsla(0%, 0%, 0%,' + opacity + ')';
-        context.fillRect(0, 0, width, height);
-
-        requestAnimationFrame(draw);
-
-      }
-
-      // when the whole page has loaded, including all dependent resources
-      window.addEventListener('load', setup);
-
-      // resize canvas when window is resized (for full browser canvas only)
-      window.addEventListener('resize', setup);
 
 // PART 2: WEBGL
 
-let camera, scene, renderer, controls, model;
+const title = document.getElementById("play");
+
+let camera, scene, renderer, controls, model, light1, object, sphere;
+
+var clock = new THREE.Clock();
+
+title.onclick = function() {
 
 function init() {
 
@@ -69,14 +16,20 @@ function init() {
   let w = window.innerWidth;
   let h = window.innerHeight;
 
-  camera = new THREE.PerspectiveCamera(20, w/h, 1, 1000);
-  camera.position.z = 55;
+  camera = new THREE.PerspectiveCamera(5, w/h, 1, 1000);
+  camera.position.z = 100;
   camera.position.y = -12;
   camera.position.x = -110;
   scene.add(camera);
 
+  var sphere = new THREE.SphereBufferGeometry( 0.1, 4, 2 );
+
+  light1 = new THREE.PointLight( 0xffffff, 2, 50 );
+	light1.add(new THREE.Mesh(sphere, new THREE.MeshBasicMaterial( { color: 0xffffff } ) ) );
+	scene.add( light1 );
+
   //get ghost white using light
-  let light = new THREE.DirectionalLight(0xffffff, 10);
+  /*let light = new THREE.DirectionalLight(0xffffff, 10);
   light.position.set(1, 1, 1);
   scene.add(light);
 
@@ -86,7 +39,7 @@ function init() {
 
   let thirdlight = new THREE.DirectionalLight(0xffffff, 10);
   thirdlight.position.set(0, 0, -1);
-  scene.add(thirdlight);
+  scene.add(thirdlight);*/
 
   //GL Transmission Format loader
   let loader = new THREE.GLTFLoader();
@@ -118,24 +71,39 @@ function init() {
 
 function animate() {
   requestAnimationFrame(animate);
-  renderer.render(scene, camera);
+  render();
 
   //animate the ghost up and down
   if (model) {
     model.position.y += vector;
     //model.rotation.y += 0.01;
 
-    if (model.position.y > 1) {
-      vector = -0.03;
+    if (model.position.y > 0.5) {
+      vector = -0.01;
     }
 
     if (model.position.y < 0) {
-      vector = 0.03;
+      vector = 0.01;
     }
   }
 
   controls.update();
 }
 
+function render() {
+  var time = Date.now() * 0.0005;
+	var delta = clock.getDelta();
+
+	if (object) object.rotation.y -= 0.5 * delta;
+
+	light1.position.x = Math.sin( time * 0.7 ) * 3;
+	light1.position.y = Math.cos( time * 0.5 ) * 4;
+	light1.position.z = Math.cos( time * 0.3 ) * 3;
+
+  renderer.render(scene, camera);
+}
+
 init();
 animate();
+
+};
